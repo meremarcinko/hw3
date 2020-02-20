fc1b8814c8197651bae4ddb7e7a713cc2734a4c3
meremarcinko <42845974+meremarcinko@users.noreply.github.com>
2020-02-19 20:39:31 -0800

    hw5
    all of hw5

--- public/javascripts/hw3_javascript.js
+++ public/javascripts/hw3_javascript.js
@@ -28,6 +28,9 @@ function order()
 		$("p").text("Thank you! Your order has been placed!");
 		$("p").append("<li>You ordered: " + quantity + " cheesecake(s) that are " + topping);
 		
+		$.post("http://localhost:3000/newOrder?quantity=" + quantity + "&topping=" + topping + "&textArea=" + textArea , function(data){
+			
+		});
 	}
 	
 	if(checker)
@@ -35,16 +38,46 @@ function order()
 		document.getElementById("table").innerHTML = "";
 		$("p").text("Thank you! Your order has been placed!");
 		$("p").append("<li>You ordered: " + quantity + " cheesecake(s) that are " + topping);
+		
+		$.post("http://localhost:3000/newOrder?quantity=" + quantity + "&topping=" + topping + "&textArea=" + textArea , function(data){
+			
+		});
 	}
 }
 
 function updateMonth(month)
 {
 	document.getElementById("drop").innerHTML = month;
-	$.post("http://localhost:3000/orders", function(data, status){
-		var json = JSON.parse(data);
-		document.getElementById("cher").innerHTML = json.a.quantity + " cherry";
-		document.getElementById("choc").innerHTML = json.b.quantity + " chocolate";
-		document.getElementById("pla").innerHTML = json.c.quantity + " plain";
-	},  'json');
+	$.post("http://localhost:3000/orders?month=" + month.toUpperCase(), function(data){
+		var num1 = 0;
+		var num2 = 0;
+		var num3 = 0;
+		
+		
+		for(var i = 0; i<data.length; i++)
+		{
+			if(data[i].TOPPING == "plain")
+			{
+				//num1 = data[i].QUANTITY + num1;
+				num1+=data[i].QUANTITY;
+			}
+			if(data[i].TOPPING == "cherry")
+			{
+				//num2 = data[i].QUANTITY + num2;
+				num2+=data[i].QUANTITY;
+			}
+			if(data[i].TOPPING == "chocolate")
+			{
+				num3+=data[i].QUANTITY;
+				//num3 = data[i].QUANTITY + num3;
+			}			
+		}
+		
+		
+		document.getElementById("cher").innerHTML = num2 + " cherry";
+		document.getElementById("pla").innerHTML = num1 + " plain";
+		document.getElementById("choc").innerHTML = num3 + " chocolate";
+	
+		
+	});
 }
--- routes/newOrder.js
+++ routes/newOrder.js
@@ -7,15 +7,18 @@ var db = require('./dbms.js');
 
 router.post('/', function(req, res, next)
 {
-	db.dbquery("SELECT * FROM ORDERS WHERE MONTH = '" + req.query.month + "'", 
+	var orderId = Math.floor(Math.random() * 9999) + 100;
+	db.dbquery("INSERT INTO ORDERS (ORDERID, MONTH, QUANTITY, TOPPING, DAY, NOTES) VALUES (" + orderId + ", 'SEP'," + req.query.quantity +", '" + req.query.topping + "' , 8, '" + req.query.textArea + "')", 
 			function(error, result){
 				if(error)
 				{	
 					console.log(error);
 					return;
 				}
-				res.json(result);
+				
 			});
 });
+
+
 
 module.exports = router;
